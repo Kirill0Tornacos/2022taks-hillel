c9707f19a48aff71776f9b1ee59a8e4b85013d58
Kirill_Bogashov <kirillmsgame@gmail.com>
2022-02-23 15:33:26 +0300

    homework1

--- src/com/hillel/homeworks/Main.java
+++ src/com/hillel/homeworks/Main.java
@@ -13,13 +13,20 @@ public class Main {
         int result = number1 + number2;
 
         for (int i = 0; i < result; i++) {
-            System.out.println("1");
+            System.out.println(result);
         }
         System.out.println("Do you want to continue or stop? press 1 - continue, press 2 - stop");
         int num1 = scanner.nextInt();
         int num2 = 1;
         if (num1 == num2) {
             System.out.println("Input number:");
+            int number3 = scanner.nextInt();
+            int number4 = scanner.nextInt();
+            int result1 = number3 + number4;
+
+            for (int i = 0; i < result1; i++) {
+                System.out.println(result1);
+            }
         } else {
             System.out.println("Your program has stopped");
         }
